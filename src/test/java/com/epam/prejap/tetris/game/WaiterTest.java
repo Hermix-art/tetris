7732c8194be02c1479624d13a29af117738a54c3
Herman Kulik <hermankulik1995@gmail.com>
2021-06-18 10:47:01 +0200

    Threshold, method modifiers, testing groups are changed
    Threshold was increased, in order to let the program not to fail the tests, when CPU of machine is working with many other processes. Testing group names were amended. WaiterTest methods modifiers were changed to package private and private.

--- src/test/java/com/epam/prejap/tetris/game/WaiterTest.java
+++ src/test/java/com/epam/prejap/tetris/game/WaiterTest.java
@@ -11,54 +11,54 @@ import org.testng.annotations.Test;
  * @author Herman Kulik
  */
 public class WaiterTest {
-    private static final int THRESHOLD = 30;
+    private static final int THRESHOLD = 50;
     private static int delay;
 
     @Factory(dataProvider = "dataMethod")
-    public WaiterTest(int newDelay) {
+    WaiterTest(int newDelay) {
         delay = newDelay;
     }
 
     @Test(timeOut = 500 + THRESHOLD, groups = "delays")
-    public void waiterShouldWaitFor500millisTime() {
+    void waiterShouldWaitFor500millisTime() {
         Waiter waiter = new Waiter(500);
         waiter.waitForIt();
     }
 
     @Test(timeOut = 400 + THRESHOLD, groups = "delays")
-    public void waiterShouldWaitFor400millisTime() {
+    void waiterShouldWaitFor400millisTime() {
         Waiter waiter = new Waiter(400);
         waiter.waitForIt();
     }
 
     @Test(timeOut = THRESHOLD, groups = "delays")
-    public void waiterShouldNotWait() {
+    void waiterShouldNotWait() {
         Waiter waiter = new Waiter(0);
         waiter.waitForIt();
     }
 
     @Test(dataProvider = "constantDelays", groups = "delays")
-    public void delayShouldStayTheSame(int score) {
+    void delayShouldStayTheSame(int score) {
         Waiter waiter = new Waiter(delay);
         waiter.speedTheGame(score);
         Assert.assertEquals(waiter.getMilliseconds(), delay, "Delay should not decrease if there are no 10 additional points of scores");
     }
 
-    @Test(groups = {"exceptions","delays"})
-    public void negativeDelayShouldNotThrowException() {
+    @Test(groups = "delays")
+    void negativeDelayShouldNotThrowException() {
         Waiter waiter = new Waiter(-100);
         waiter.waitForIt();
     }
 
     @Test(dataProvider = "variableDelays", groups = "delays")
-    public void delayShouldChangeBy100(int score) {
+    void delayShouldChangeBy100(int score) {
         Waiter waiter = new Waiter(delay);
         waiter.speedTheGame(score);
         Assert.assertEquals(waiter.getMilliseconds(), delay - 100, "Delay should decrease every 10 points of scores");
     }
 
     @DataProvider
-    public static Object[][] dataMethod() {
+    private static Object[][] dataMethod() {
         return new Object[][]{
                 {500},
                 {400},
@@ -91,7 +91,7 @@ public class WaiterTest {
 
 
     @DataProvider
-    public static Object[][] variableDelays() {
+    private static Object[][] variableDelays() {
         return new Object[][]{
                 {10},
                 {20},
